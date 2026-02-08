49bb6ffa781329d17832a4ec445f988c93368209
oneksson <eduardochristianhuanca63@gmail.com>
2026-02-08 12:51:43 -0300

    se borra un import innecesario

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -5,7 +5,6 @@ import io.qameta.allure.Epic;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import steps.LobbySteps;
 import steps.LoginSteps;
 import steps.NavSteps;
 
